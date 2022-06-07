f8189d493ddfec4a8a40ca57586c248c9a4df7ed
Madhu K <69120069+stepin104734@users.noreply.github.com>
2022-06-07 19:50:33 +0530

    Update delimiter.c
    added comments

--- delimiter.c
+++ delimiter.c
@@ -5,11 +5,11 @@ int main ()
   char string[50] ="my_name_is:edcast;future'skills";
   char *p;
 
-  p = strtok (string,"_:;'");
+  p = strtok (string,"_:;'");//this return's the first token of the string . i.e "my"
   while (p!= NULL)
   {
     printf ("%s\n",p);
-    p = strtok(NULL, "_:;'");
+    p = strtok(NULL, "_:;'");//NULL argument tell strtok function to continue tokenizing the previous string hence returns string "name"
   }
   return 0;
 }
